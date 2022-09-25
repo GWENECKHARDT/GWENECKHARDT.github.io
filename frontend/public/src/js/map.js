25e1bb7d4ab239fe0db72588aa417a382ff3222f
GWENECKHARDT <65242718+GWENECKHARDT@users.noreply.github.com>
2022-09-25 18:26:09 +0200

    Update map.js

--- frontend/public/src/js/map.js
+++ frontend/public/src/js/map.js
@@ -22,8 +22,6 @@ fetch('http://localhost:3000/posts')
                 locations[0]=[10, 20]
                 locations[1]=[20, 30]
                 locations[2]=[data[11]["longitude"], data[11]["latitude"]]*/
-        locations[0]=[13.4105300, 52.5243700]
-        locations[1]=[9.993682, 53.551086]
         console.log('Berlin: ', locations[11]);
         console.log('Array: ',locations);
         showLocations();
@@ -35,6 +33,8 @@ fetch('http://localhost:3000/posts')
                     console.log('From cache: ', data);
                 })
         }
+        locations[0]=[13.4105300, 52.5243700];
+        locations[1]=[9.993682, 53.551086];
     });
 
 function createMarker()
